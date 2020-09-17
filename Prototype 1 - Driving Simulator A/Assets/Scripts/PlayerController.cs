15844e680691324c5781ebb207c26391b14394e7
Powell <preston.powell@nebo.edu>
2020-09-17 12:31:27 -0600

    Fixed gitignore and have added the Unity project

---
+++ Prototype 1 - Driving Simulator A/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Access Modifier Type Name Operator Value
+    private int speed = 20;
+    private float turnSpeed = 50;
+
+    private float horizontalInput;
+    private float forwardInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+        // Move the vehicle forward & back based on forwardInput
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        // Rotates vehicle left and right based on horizontalInput
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+    }
+}
