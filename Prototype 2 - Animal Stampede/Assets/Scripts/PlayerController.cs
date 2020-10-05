840c27c5662b5ac61a07038deaf1b01054bb7ef6
Powell <preston.powell@nebo.edu>
2020-10-05 12:42:11 -0600

    Added Prototype 2 and scripted player movement

---
+++ Prototype 2 - Animal Stampede/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float horizontalInput;
+    public float speed = 10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * Time.deltaTime * horizontalInput * speed);
+        // Constrains Players Movement X to the left at -10
+        if(transform.position.x < -10)
+        {
+            transform.position = new Vector3(-10, transform.position.y, transform.position.z);
+
+        }
+        // Constrains Players Movement X to the right at 10
+        if (transform.position.x > 10)
+        {
+            transform.position = new Vector3(10, transform.position.y, transform.position.z);
+
+        }
+
+    }
+}
