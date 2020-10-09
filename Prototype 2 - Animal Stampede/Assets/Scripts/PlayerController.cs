8cafe210bd55b726eddc78ad140a1954d832ec3c
Powell <preston.powell@nebo.edu>
2020-10-09 15:21:20 -0600

    Updated game

--- Prototype 2 - Animal Stampede/Assets/Scripts/PlayerController.cs
+++ Prototype 2 - Animal Stampede/Assets/Scripts/PlayerController.cs
@@ -6,6 +6,9 @@ public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
     public float speed = 10.0f;
+    public float xRange = 15.0f;
+
+    public GameObject foodProjectile;
 
     // Start is called before the first frame update
     void Start()
@@ -18,17 +21,25 @@ public class PlayerController : MonoBehaviour
     {
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * Time.deltaTime * horizontalInput * speed);
+
         // Constrains Players Movement X to the left at -10
-        if(transform.position.x < -10)
+        if(transform.position.x < -xRange)
         {
-            transform.position = new Vector3(-10, transform.position.y, transform.position.z);
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
 
         }
-        // Constrains Players Movement X to the right at 10
-        if (transform.position.x > 10)
-        {
-            transform.position = new Vector3(10, transform.position.y, transform.position.z);
 
+        // Constrains Players Movement X to the right at 10
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+
+            Instantiate(foodProjectile, transform.position, foodProjectile.transform.rotation);
         }
 
     }
