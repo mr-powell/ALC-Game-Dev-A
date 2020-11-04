3cdc66711a08d8dadf951b31e58c21f48ebd2088
Powell <preston.powell@nebo.edu>
2020-11-04 09:07:37 -0700

    Added Obstacle Object and created an Obstacle spawn. Jumps are constrained to one after touching the ground.

--- Prototype 3- Run and Jump/Assets/Scripts/PlayerController.cs
+++ Prototype 3- Run and Jump/Assets/Scripts/PlayerController.cs
@@ -7,22 +7,27 @@ public class PlayerController : MonoBehaviour
     private Rigidbody playerRb;
     public float jumpForce;
     public float gravityModifier;
+    public bool isOnGround = true;
 
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
-        Physics.gravity *= gravityModifier;
-
-        //Physics.gravity = Physics.gravity * gravityModifier; 
+        Physics.gravity *= gravityModifier;        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
         {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
         } 
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        isOnGround = true;
     }
 }
