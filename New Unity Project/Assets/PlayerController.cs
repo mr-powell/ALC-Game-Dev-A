54fc24ec6c5fcfa7184de06a3454af773bf9f3a1
Powell <preston.powell@nebo.edu>
2020-11-13 12:38:09 -0700

    Game Update

---
+++ New Unity Project/Assets/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float speed = 10f;
+    private Animator playerAnim;
+
+    public CharacterController characterController;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerAnim = GetComponent<Animator>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Move();
+        
+    }
+
+    private void Move()
+    {
+        
+        float hInput = Input.GetAxis("Horizontal");
+        float vInput = Input.GetAxis("Vertical");
+        playerAnim.SetBool("isWalking", vInput != 0 || hInput != 0);
+
+        Vector3 move = transform.forward * vInput + transform.right * hInput;
+        characterController.Move(speed * Time.deltaTime * move);
+    }
+}
--- Prototype 3- Run and Jump/Assets/Scripts/PlayerController.cs
+++ Prototype 3- Run and Jump/Assets/Scripts/PlayerController.cs
@@ -10,20 +10,25 @@ public class PlayerController : MonoBehaviour
     public bool isOnGround = true;
     public bool gameOver = false;
 
+    private Animator playerAnim;
+    public ParticleSystem explosionParticle;
+
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
-        Physics.gravity *= gravityModifier;        
+        Physics.gravity *= gravityModifier;
+        playerAnim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
         {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
+            playerAnim.SetTrigger("Jump_trig");
         } 
     }
 
@@ -37,8 +42,13 @@ public class PlayerController : MonoBehaviour
         }
         else if(collision.gameObject.CompareTag("Obstacle"))
         {
-            gameOver = true;
+            
             Debug.Log("Game Over!");
+            gameOver = true;
+            playerAnim.SetBool("Death_b", true);
+            playerAnim.SetInteger("DeathType", 1);
+            explosionParticle.Play();
+
         }
     }
 }
